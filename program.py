a463adee70f2b7a87e6882c2ed6f49edbd970a37
m0rfey <ddiversantt@gmail.com>
2016-02-22 15:03:17 +0200

    Доводка работы программы

--- program.py
+++ program.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from PyQt4 import QtCore, QtGui, uic
-import sys, ui_window
+import os, sys, ui_window
 
 class MyWindow(QtGui.QWidget):
     def __init__(self, parent=None):
@@ -9,14 +9,22 @@ class MyWindow(QtGui.QWidget):
         self.ui = ui_window.Ui_Enigma()
         self.ui.setupUi(self)
 
-        self.file = open('conf.config')
-        self.spp = []
-        self.symbols(0)
-        self.ui.spinBox_quantity_symbol.setProperty('value', 2)
+        self.directory =os.path.dirname('config/')
+        self.name_file ='.config'
 
+        self.file = open(self.directory +'/'+ self.name_file)
+        self.spp = []
+
+        default_lang = 0
+        default_len = 2
+
+        self.lokomotiv(default_len)
+
+        self.option = default_lang
+        self.symbols(default_lang)
+        self.ui.spinBox_quantity_symbol.setProperty('value', 0)
         for ex in self.file.readlines():
             self.spp.append(ex)
-            #print(self.spp)
 
         QtCore.QObject.connect(self.ui.pushButton_save, QtCore.SIGNAL("clicked()"), self.create_file)
         QtCore.QObject.connect(self.ui.spinBox_quantity_symbol, QtCore.SIGNAL("valueChanged(int)"), self.lokomotiv)
@@ -113,37 +121,122 @@ class MyWindow(QtGui.QWidget):
              self.ui.lineEdit_sym_5.text(),self.ui.lineEdit_sym_6.text(),self.ui.lineEdit_sym_7.text(),self.ui.lineEdit_sym_8.text(),self.ui.lineEdit_sym_9.text()]
 
         for bi in b:
-            self.k.append(bi)
+            if not bi:
+                pass
+            else:
+                self.k.append(bi)
         for ni in n:
-            self.k.append(ni)
+            if not ni:
+                pass
+            else:
+                self.k.append(ni)
         for si in s:
-            self.k.append(si)
+            if not si:
+                pass
+            else:
+                self.k.append(si)
 
         self.SaveSetings(self.k)
 
     def symbols(self, option):
-        option = self.ui.comboBox_lang.currentIndex()
-        if option == 0:
+        self.option = self.ui.comboBox_lang.currentIndex()
+
+        if self.option == 0:
+            self.name_lan= 'ua'
             print('UA')
-            symbol_ua = [u'а', u'б', u'в', u'г', u'ґ', u'д', u'е', u'є', u'ж', u'з', u'и', u'і', u'ї', u'й', u'к', u'л',
-                         u'м', u'н', u'о', u'п', u'р', u'с', u'т', u'у', u'ф', u'х', u'ц', u'ч', u'ш', u'щ', u'ь', u'ю',
-                         u'я', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '!', '?', ',', '.', ':', ';',
+            symbol_ua = ['а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л',
+                         'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю',
+                         'я', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '!', '?', ',', '.', ':', ';',
                          '(', ')', '@', "'", '\n']
             self.symbol = symbol_ua
-        elif option == 1:
+
+            self.ui.lineEdit_26.setDisabled(False)
+            self.ui.lineEdit_27.setDisabled(False)
+            self.ui.lineEdit_28.setDisabled(False)
+            self.ui.lineEdit_29.setDisabled(False)
+            self.ui.lineEdit_30.setDisabled(False)
+            self.ui.lineEdit_31.setDisabled(False)
+            self.ui.lineEdit_32.setDisabled(False)
+
+            self.ui.label_0.setText((symbol_ua[0]+':').upper()),self.ui.label_1.setText((symbol_ua[1]+':').upper())
+            self.ui.label_2.setText((symbol_ua[2]+':').upper()),self.ui.label_3.setText((symbol_ua[3]+':').upper())
+            self.ui.label_4.setText((symbol_ua[4]+':').upper()),self.ui.label_5.setText((symbol_ua[5]+':').upper())
+            self.ui.label_6.setText((symbol_ua[6]+':').upper()),self.ui.label_7.setText((symbol_ua[7]+':').upper())
+            self.ui.label_8.setText((symbol_ua[8]+':').upper()),self.ui.label_9.setText((symbol_ua[9]+':').upper())
+            self.ui.label_10.setText((symbol_ua[10]+':').upper()),self.ui.label_11.setText((symbol_ua[11]+':').upper())
+            self.ui.label_12.setText((symbol_ua[12]+':').upper()),self.ui.label_13.setText((symbol_ua[13]+':').upper())
+            self.ui.label_14.setText((symbol_ua[14]+':').upper()),self.ui.label_15.setText((symbol_ua[15]+':').upper())
+            self.ui.label_16.setText((symbol_ua[16]+':').upper()),self.ui.label_17.setText((symbol_ua[17]+':').upper())
+            self.ui.label_18.setText((symbol_ua[18]+':').upper()),self.ui.label_19.setText((symbol_ua[19]+':').upper())
+            self.ui.label_20.setText((symbol_ua[20]+':').upper()), self.ui.label_21.setText((symbol_ua[21]+':').upper())
+            self.ui.label_22.setText((symbol_ua[22]+':').upper()),self.ui.label_23.setText((symbol_ua[23]+':').upper())
+            self.ui.label_24.setText((symbol_ua[24]+':').upper()),self.ui.label_25.setText((symbol_ua[25]+':').upper())
+            self.ui.label_26.setText((symbol_ua[26]+':').upper()),self.ui.label_27.setText((symbol_ua[27]+':').upper())
+            self.ui.label_28.setText((symbol_ua[28]+':').upper()),self.ui.label_29.setText((symbol_ua[29]+':').upper())
+            self.ui.label_30.setText((symbol_ua[30]+':').upper()),self.ui.label_31.setText((symbol_ua[31]+':').upper())
+            self.ui.label_32.setText((symbol_ua[32]+':').upper()),self.ui.label_num_0.setText(symbol_ua[33]+':')
+            self.ui.label_num_1.setText(symbol_ua[34]+':'),self.ui.label_num_2.setText(symbol_ua[35]+':')
+            self.ui.label_num_3.setText(symbol_ua[36]+':'),self.ui.label_num_4.setText(symbol_ua[37]+':')
+            self.ui.label_num_5.setText(symbol_ua[38]+':'),self.ui.label_num_6.setText(symbol_ua[39]+':')
+            self.ui.label_num_7.setText(symbol_ua[40]+':'),self.ui.label_num_8.setText(symbol_ua[41]+':')
+            self.ui.label_num_9.setText(symbol_ua[42]+':'),self.ui.label_sym_0.setText(symbol_ua[43]+':')
+            self.ui.label_sym_1.setText(symbol_ua[44]+':'),self.ui.label_sym_2.setText(symbol_ua[45]+':')
+            self.ui.label_sym_3.setText(symbol_ua[46]+':'),self.ui.label_sym_4.setText(symbol_ua[47]+':')
+            self.ui.label_sym_5.setText(symbol_ua[48]+':'),self.ui.label_sym_6.setText(symbol_ua[49]+':')
+            self.ui.label_sym_7.setText(symbol_ua[50]+':'),self.ui.label_sym_8.setText(symbol_ua[51]+':')
+            self.ui.label_sym_9.setText(symbol_ua[52]+':'),self.ui.label_sym_10.setText(symbol_ua[53]+':')
+            self.ui.label_sym_11.setText('/n:')
+
+        elif self.option == 1:
+            self.name_lan = 'en'
             print('EU')
             symbol_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
                          'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
                          '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '!', '?', ',', '.', ':', ';', '(', ')', '@', "'", '\n']
             self.symbol = symbol_en
-        elif option == 2:
+
+            self.ui.label_26.setText(''),self.ui.label_27.setText('')
+            self.ui.label_28.setText(''),self.ui.label_29.setText('')
+            self.ui.label_30.setText(''),self.ui.label_31.setText('')
+            self.ui.label_32.setText(''),self.ui.lineEdit_26.setDisabled(True)
+            self.ui.lineEdit_27.setDisabled(True),self.ui.lineEdit_28.setDisabled(True)
+            self.ui.lineEdit_29.setDisabled(True),self.ui.lineEdit_30.setDisabled(True)
+            self.ui.lineEdit_31.setDisabled(True),self.ui.lineEdit_32.setDisabled(True)
+
+            self.ui.label_0.setText((symbol_en[0]+':').upper()),self.ui.label_1.setText((symbol_en[1]+':').upper())
+            self.ui.label_2.setText((symbol_en[2]+':').upper()),self.ui.label_3.setText((symbol_en[3]+':').upper())
+            self.ui.label_4.setText((symbol_en[4]+':').upper()),self.ui.label_5.setText((symbol_en[5]+':').upper())
+            self.ui.label_6.setText((symbol_en[6]+':').upper()),self.ui.label_7.setText((symbol_en[7]+':').upper())
+            self.ui.label_8.setText((symbol_en[8]+':').upper()),self.ui.label_9.setText((symbol_en[9]+':').upper())
+            self.ui.label_10.setText((symbol_en[10]+':').upper()),self.ui.label_11.setText((symbol_en[11]+':').upper())
+            self.ui.label_12.setText((symbol_en[12]+':').upper()),self.ui.label_13.setText((symbol_en[13]+':').upper())
+            self.ui.label_14.setText((symbol_en[14]+':').upper()),self.ui.label_15.setText((symbol_en[15]+':').upper())
+            self.ui.label_16.setText((symbol_en[16]+':').upper()),self.ui.label_17.setText((symbol_en[17]+':').upper())
+            self.ui.label_18.setText((symbol_en[18]+':').upper()),self.ui.label_19.setText((symbol_en[19]+':').upper())
+            self.ui.label_20.setText((symbol_en[20]+':').upper()),self.ui.label_21.setText((symbol_en[21]+':').upper())
+            self.ui.label_22.setText((symbol_en[22]+':').upper()),self.ui.label_23.setText((symbol_en[23]+':').upper())
+            self.ui.label_24.setText((symbol_en[24]+':').upper()),self.ui.label_25.setText((symbol_en[25]+':').upper())
+            self.ui.label_num_0.setText(symbol_en[26]+':'),self.ui.label_num_1.setText(symbol_en[27]+':')
+            self.ui.label_num_2.setText(symbol_en[28]+':'),self.ui.label_num_3.setText(symbol_en[29]+':')
+            self.ui.label_num_4.setText(symbol_en[30]+':'),self.ui.label_num_5.setText(symbol_en[31]+':')
+            self.ui.label_num_6.setText(symbol_en[32]+':'),self.ui.label_num_7.setText(symbol_en[33]+':')
+            self.ui.label_num_8.setText(symbol_en[34]+':'),self.ui.label_num_9.setText(symbol_en[35]+':')
+            self.ui.label_sym_0.setText(symbol_en[36]+':'),self.ui.label_sym_1.setText(symbol_en[37]+':')
+            self.ui.label_sym_2.setText(symbol_en[38]+':'),self.ui.label_sym_3.setText(symbol_en[39]+':')
+            self.ui.label_sym_4.setText(symbol_en[40]+':'),self.ui.label_sym_5.setText(symbol_en[41]+':')
+            self.ui.label_sym_6.setText(symbol_en[42]+':'),self.ui.label_sym_7.setText(symbol_en[43]+':')
+            self.ui.label_sym_8.setText(symbol_en[44]+':'),self.ui.label_sym_9.setText(symbol_en[45]+':')
+            self.ui.label_sym_10.setText(symbol_en[46]+':'),self.ui.label_sym_11.setText('/n:')
+
+        elif self.option == 2:
+            self.name_lan = 'ru'
             print('RU')
             symbol_ru = []
             self.symbol = symbol_ru
 
     def encrypt(self):
         lst = []
-        file = open('conf.config')
+        file = open(self.directory +'/'+self.name_lan+ self.name_file)
         text_encrypt = self.ui.textEdit_input_encrypt.toPlainText()
         if not text_encrypt:
             self.ui.textEdit_input_encrypt.setStyleSheet("color: rgb(255, 0, 255);")
@@ -187,10 +280,21 @@ class MyWindow(QtGui.QWidget):
 
     ''' Save settings to file '''
     def SaveSetings(self, k):
-        f = open('.config', "w", encoding="utf-8")
-        for z in k:
-            f.write(str("%s\n" % z))
-        f.close()
+        if self.option == 0:
+            f = open(self.directory +'/'+ 'ua' + self.name_file, "w")#, encoding="utf-8")
+            for z in k:
+                f.write(str("%s\n" % z))
+            f.close()
+        elif self.option == 1:
+            f = open(self.directory +'/'+ 'en' + self.name_file, "w")#, encoding="utf-8")
+            for z in k:
+                f.write(str("%s\n" % z))
+            f.close()
+        elif self.option == 2:
+            f = open(self.directory +'/'+ 'ru' + self.name_file, "w")#, encoding="utf-8")
+            for z in k:
+                f.write(str("%s\n" % z))
+            f.close()
         return
 
 if __name__ == '__main__':
